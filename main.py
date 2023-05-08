2fc1d2a306233af3cc43519a22a43be9fff87174
ddd <daiman4444@gmail.com>
2023-05-08 23:32:54 +0600

    hueta

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
-import sys
-from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import (
-QApplication, QWidget,
-)
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+import operator
 from test_app import Ui_MainWindow
 
-class
 
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __int__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    app.setApplicationName('Calc')
+    window = MainWindow()
+    window.show()
+    app.exec_()
