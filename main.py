875a789b852b861e4a9bea558321a4ae111f8bd0
ddd <daiman4444@gmail.com>
2023-05-08 22:20:51 +0600

    started

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import (
+QApplication, QWidget,
+)
+from test_app import Ui_MainWindow
+
+class
+
