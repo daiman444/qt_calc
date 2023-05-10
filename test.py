23994d0ebde417e3b221b1890b376cedd6772a33
ddd <daiman4444@gmail.com>
2023-05-10 19:18:11 +0600

    added labels for input and result

--- test.py
+++ test.py
@@ -3,6 +3,8 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QVBoxLayout, QLabel
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
 from PyQt5.QtCore import QCoreApplication
 
 
@@ -12,11 +14,23 @@ class Example(QWidget):
         self.pb = QPushButton
         self.pb_pos_x = 20
         self.pb_pos_y = 30
+        self.font_result = QFont('Arial', 16, 3)
+        self.font_input = QFont('Arial', 25, 1)
+
         self.initUI()
 
     def initUI(self):
         grid = QGridLayout()
-        #self.setLayout(grid)
+        result_string = QLabel('Result')
+        result_string.setFont(self.font_result)
+        result_string.setStyleSheet('color: orange; background-color: rgb(64, 64, 64)')
+        result_string.setAlignment(Qt.AlignRight)
+
+        input_string = QLabel('0')
+        input_string.setFont(self.font_input)
+        input_string.setStyleSheet('color: rgb(153, 255, 153); background-color: rgb(64, 64, 64)')
+        input_string.setAlignment(Qt.AlignRight)
+
 
         names = ['C', '+/-', '<<', '*',
                  '7', '8', '9', '/',
@@ -30,16 +44,22 @@ class Example(QWidget):
         for position, name in zip(positions, names):
             if name == '':
                 continue
+            if name == '=':
+                pb = QPushButton(name)
+                grid.addWidget(pb, *position, 1, 2, )
+                break
             pb = QPushButton(name)
             grid.addWidget(pb, *position)
 
         v_layout = QVBoxLayout()
-        v_layout.addStretch(2)
+
+        v_layout.addWidget(result_string)
+        v_layout.addWidget(input_string)
         v_layout.addLayout(grid)
 
         self.setLayout(v_layout)
         self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('Foo')
+        self.setWindowTitle('Calc')
         self.show()
 
 
